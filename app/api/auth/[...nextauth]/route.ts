f893e2c768fb754362366ef4b19de815ca71505f
utkarshgupta04092003 <utkarshgupta04092003@gmail.com>
2026-05-04 23:25:25 +0530

    Fix Vercel Build Error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,5 +2,6 @@ import { authOptions } from "@/lib/auth";
 import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
+export const dynamic = "force-dynamic";
 
 export { handler as GET, handler as POST };
